b93b57e55bb2b753588837843ceed633f8fd8d33
KeriPMiller <KeriPMiller@users.noreply.github.com>
2017-09-30 13:09:36 -0400

    completed user model setup

--- models/user.js
+++ models/user.js
@@ -3,8 +3,43 @@ const db = new Sequelize('postgres://localhost:5432/sequelize_practice', { loggi
 
 const User = db.define('user', {
 // YOUR CODE HERE...
+  first:{
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  last:{
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  age:{
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 18
+      }
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  },
+  bio: {
+    type: Sequelize.TEXT
+  }
 }, {
-// ...AND HERE
+// getters
+  getterMethods :{
+    fullName() {
+      return this.first + ' ' + this.last;
+    }
+  }
+
 });
+
+User.prototype.haveBirthday = function(){
+
+}
 
 module.exports = User;
